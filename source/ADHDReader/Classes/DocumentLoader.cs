fd57d8c5be69c539b7e4eb4d6aa8a52896df474b
Henrik Henriksson <56026945+henrikhenriksson@users.noreply.github.com>
2024-08-08 09:14:24 +0200

    Using EVO-PDFtoText for text extraction

--- source/ADHDReader/Classes/DocumentLoader.cs
+++ source/ADHDReader/Classes/DocumentLoader.cs
@@ -1,31 +1,59 @@
 ﻿using System.IO;
 using System.Text;
-using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas.Parser;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using EvoPdf.PdfToText;
+
 
 namespace ADHDReader.Classes
 {
     internal class DocumentLoader
     {
-        public static string LoadPDF(string filePath)
+        //public static string LoadPDF(string filePath)
+        //{
+
+        //    using PdfReader reader = new(filePath);
+        //    using PdfDocument pdfDocument = new(reader);
+        //    var strategy = new LocationTextExtractionStrategy();
+
+        //    StringBuilder processed = new StringBuilder();
+            
+        //    for (int i = 1; i<=pdfDocument.GetNumberOfPages(); ++i)
+        //    {
+        //        var page = pdfDocument.GetPage(i);
+        //        string text = PdfTextExtractor.GetTextFromPage(page, strategy);
+        //        processed.AppendLine(text);
+        //    }
+        //    return processed.ToString();
+
+        //}
+        public static string ReadPdF(string filePath)
         {
 
-            using PdfReader reader = new PdfReader(filePath);
-            using PdfDocument pdfDocument = new(reader);
-            var strategy = new LocationTextExtractionStrategy();
+            int startPageNumber = 0;
+            int endPageNumber = 0;
+            PdfToTextConverter converter = new PdfToTextConverter();
+            converter.LicenseKey = "ujQlNSAgNSU1IzslNSYkOyQnOywsLCw1JQ==";
+          //  TextLayout textLayout = TextLayout.ReadingLayout;
+            System.Text.Encoding textEncoding = Encoding.UTF8;
+            converter.MarkPageBreaks = true;
 
-            StringBuilder processed = new StringBuilder();
-            
-            for (int i = 1; i<=pdfDocument.GetNumberOfPages(); ++i)
+            string extractedText;
+            try
             {
-                var page = pdfDocument.GetPage(i);
-                string text = PdfTextExtractor.GetTextFromPage(page, strategy);
-                processed.Append(text);
-            }
-            return processed.ToString();
+                // read the PDF file in a memory buffer
+                byte[] sourcePdfBytes = System.IO.File.ReadAllBytes(filePath);
 
+                // extract text from PDF
+                extractedText = converter.ConvertToText(sourcePdfBytes, startPageNumber, endPageNumber);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(String.Format("An error occurred. {0}", ex.Message));
+            }
+            return extractedText;
         }
+
+        
+
 
     }
 }
