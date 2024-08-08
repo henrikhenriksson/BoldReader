bdac80c932168ba3adbbaa8d6ca80a192221a3b1
Henrik Henriksson <56026945+henrikhenriksson@users.noreply.github.com>
2024-08-08 09:15:12 +0200

    Implemented Regex split to preserve formatting

--- source/ADHDReader/Classes/TextProcessor.cs
+++ source/ADHDReader/Classes/TextProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ADHDReader.Classes
@@ -9,39 +10,14 @@ namespace ADHDReader.Classes
     public static class TextProcessor
     {
 
-        public static String ProcessText(string input)
+        public static List<string> ProcessText(string input)
         {
-            var words = input.Split(' ');
-            var processedWords= words.Select(FormatWord);
-            return string.Join(" ", processedWords);
-        }
+             //  var words = input.Split(' ').ToList();
+
+            var words = Regex.Split(input, @"(\s+|\b)").ToList();
 
 
-        private static string FormatWord(string word)
-        {
-            if (word.Length <= 4)
-            {
-                return Bolden(word, 1, word.Length - 2);
-
-
-            } else
-            {
-                return Bolden(word, 2, word.Length - 3);
-            }
-        }
-
-        private static string Bolden(string word, int startBold, int endBold)
-        {
-            if (word.Length > startBold + endBold + 1)
-            {
-                return word;
-            }
-
-            var boldStart = word.Substring(0, startBold);
-            var boldEnd = word.Substring(word.Length - endBold);
-            var middle = word.Substring(startBold, word.Length - (startBold + endBold));
-
-            return $"<b>{boldStart}</b>{middle}<b>{boldEnd}</b>"; ;
+            return words;
         }
 
     }
